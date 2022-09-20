cd549ebbed6803d95199b4b2c190e02122175873
Дмитрий <rotweis87@gmail.com>
2022-09-20 20:25:41 +0300

    Добавил решение задачи 52

---
+++ Zadacha_52/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+//элементов в каждом столбце.
+
+void massiv()
+{
+int m = new Random().Next(5, 5);
+int n = new Random().Next(5, 5);
+
+double[,] table = new double[m,n];
+
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        table[i,j] = new Random().Next(-20, 20);
+    }
+}
+
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        Console.Write($"{table[i,j]}   ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое в кждом столбце:");
+for (int j = 0; j < table.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        sum += table[i, j];
+    }
+    Console.Write($"{ Math.Round(sum / table.GetLength(0), 2)} ");
+}
+}
+massiv();
